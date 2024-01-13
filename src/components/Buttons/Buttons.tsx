ea08a8fa18d22af6803de19f996b1e099ecd6854
Mirsaid Rashidov <mirsaidrasidov1@gmail.com>
2024-01-13 09:55:51 +0600

    added br between buttons

--- src/components/Buttons/Buttons.tsx
+++ src/components/Buttons/Buttons.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Buttons.css';
 
 interface ButtonsProps {
 	onAdd: (ingredientName: string) => void;
@@ -7,12 +8,15 @@ interface ButtonsProps {
 
 const Buttons: React.FC<ButtonsProps> = ({ onAdd, onRemoveAll }) => {
 	return (
-		<div>
+		<div className='Buttons'>
 			<button onClick={() => onAdd('Meat')}>Meat</button>
+			<br />
 			<button onClick={() => onAdd('Cheese')}>Cheese</button>
+			<br />
 			<button onClick={() => onAdd('Salad')}>Salad</button>
+			<br />
 			<button onClick={() => onAdd('Bacon')}>Bacon</button>
-
+			<br />
 			<button onClick={onRemoveAll}>Удалить все ингредиенты</button>
 		</div>
 	);
