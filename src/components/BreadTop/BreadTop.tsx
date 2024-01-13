a6eef85390bdcedbb9ecfd7f7ecbe268371b5004
Mirsaid Rashidov <mirsaidrasidov1@gmail.com>
2024-01-13 09:39:22 +0600

    Created and add BreadTop component when i imported Seeds

---
+++ src/components/BreadTop/BreadTop.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './BreadTop.css';
+import Seeds from '../Seeds/Seeds';
+
+const BreadTop: React.FC = () => {
+	return (
+		<div className='BreadTop'>
+			<Seeds />
+		</div>
+	);
+};
+
+export default BreadTop;
